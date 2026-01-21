c7407c86b2cd2bf8d03abce030cc1a738544a14b
kl2400032141 <2400032141@kluniversity.in>
2026-01-21 10:41:46 +0530

    21-01-2026 Added DI-ECommerce-Autowired project

---
+++ DI-ECommerce-Autowired/src/main/java/com/klu/DiECommerceAutowinedApplication.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiECommerceAutowinedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiECommerceAutowinedApplication.class, args);
+	}
+
+}
