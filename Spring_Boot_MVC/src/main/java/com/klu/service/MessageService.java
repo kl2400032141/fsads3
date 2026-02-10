bf2e06310cb10a1b0638e5c5faea11f2b640fa4a
kl2400032141 <2400032141@kluniversity.in>
2026-02-10 08:30:19 +0530

    10-02-2026 Added Spring Boot MVC project

---
+++ Spring_Boot_MVC/src/main/java/com/klu/service/MessageService.java
@@ -0,0 +1,9 @@
+package com.klu.service;
+import org.springframework.stereotype.Service;
+@Service
+public class MessageService {
+  public String getMessage() {
+    return "Welcome to spring boot application";
+  }
+  
+}
